d0e4c3f1ac6b2c9b301a0dd380de9d76346a34c8
gcglynn <gcglynn@gmail.com>
2016-04-04 14:29:01 -0700

    Initial commit

---
+++ app/src/main/java/tcss450/uw/edu/aboutme/FirstActivity.java
@@ -0,0 +1,113 @@
+package tcss450.uw.edu.aboutme;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "edu.uw.tcss450.aboutme.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Text button */
+    public void displayText(View view) {
+        Intent intent = new Intent(this, DisplayTextActivity.class);
+        String message = "My name is Gabrielle!";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Text button */
+    public void displayImage(View view) {
+        Intent intent = new Intent(this, DisplayImageActivity.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Text button */
+    public void displayWeb(View view) {
+        Uri webpage = Uri.parse("http://developer.android.com/index.html");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(webIntent);
+    }
+
+    /** Called when the user clicks the Text button */
+    public void displayToast(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /** Called when the user clicks the Text button */
+    public void displayDialog(View view) {
+        Context context = getApplicationContext();
+        AlertDialog builder = new AlertDialog.Builder(context)
+                .setTitle("Are you sure")
+                .setMessage("Do you really want to press this button?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();;
+//        builder.setMessage(R.string.dialog_message)
+//                .setTitle(R.string.dialog_title);
+        //AlertDialog dialog = builder.create();
+    }
+}
